0d2aa88bf696aa9b921a6666eb2d0ea9895a5b6c
Thibault <thib-17@orange.fr>
2018-06-01 14:02:07 +0200

    Add redis pub/sub

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ const express = require("express");
 const http = require("http");
 const WebSocket = require("ws");
 const uuidv4 = require("uuid/v4");
+const redis = require("redis");
+const publisher = redis.createClient();
+const client = redis.createClient();
 
 const app = express();
 
@@ -30,6 +33,10 @@ function subscribe(socket, channel) {
 
     socketsPerChannels.set(channel, socketSubscribed);
     channelsPerSocket.set(socket, channelSubscribed);
+
+    if (socketSubscribed.size === 1) {
+        client.subscribe(channel);
+    }
 }
 
 /*
@@ -44,6 +51,10 @@ function unsubscribe(socket, channel) {
 
     socketsPerChannels.set(channel, socketSubscribed);
     channelsPerSocket.set(socket, channelSubscribed);
+
+    if (socketSubscribed.size === 0) {
+        client.unsubscribe(channel);
+    }
 }
 
 /*
@@ -57,20 +68,21 @@ function unsubscribeAll(socket) {
     });
 }
 
-/*
- * Broadcast a message to all sockets connected to this server.
- */
-function broadcastToSockets(channel, data) {
+function broadcast(channel, data) {
+    publisher.publish(channel, data);
+}
+
+client.on("message", (channel, message) => {
     const socketSubscribed = socketsPerChannels.get(channel) || new Set();
 
-    socketSubscribed.forEach(client => {
-        client.send(data);
+    socketSubscribed.forEach(socket => {
+        socket.send(message);
     });
-}
+});
 
 // Broadcast message from client
 wss.on("connection", ws => {
-    ws.on('close', () => {
+    ws.on("close", () => {
         unsubscribeAll(ws);
     });
 
@@ -78,11 +90,11 @@ wss.on("connection", ws => {
         const message = JSON.parse(data.toString());
 
         switch (message.type) {
-            case 'subscribe':
+            case "subscribe":
                 subscribe(ws, message.channel);
                 break;
             default:
-                broadcastToSockets(message.channel, data);
+                broadcast(message.channel, data);
                 break;
         }
     });
@@ -104,5 +116,5 @@ app.get("/:channel", (req, res, next) => {
 app.use(express.static(PUBLIC_FOLDER));
 
 server.listen(PORT, () => {
-    console.log(`Server started on port ${server.address().port}`);
+    console.log(`Server started on http://localhost:${server.address().port}`);
 });
